d99b9fed691a8920ee8d833c0a03dd9e47dcc57e
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-10-07 16:37:12 -0700

    Established `Editor` area
    Placing the editor in an area should help make the project easier to integrate with existing web applications in the future.

---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Controllers/EditorController.cs
@@ -0,0 +1,85 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ignia.Topics.Editor.Models;
+using Ignia.Topics.Repositories;
+
+namespace Ignia.Topics.Editor.Mvc.Controllers {
+
+  /*============================================================================================================================
+  | CLASS: EDITOR CONTROLLER
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides data access and processing for editor related functions.
+  /// </summary>
+  public class EditorController : Controller {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private                     ITopicRepository                _topicRepository                = null;
+    private                     Topic                           _currentTopic                   = null;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a Topic Controller with necessary dependencies.
+    /// </summary>
+    /// <returns>A topic controller for loading OnTopic views.</returns>
+    public EditorController(ITopicRepository topicRepository, Topic currentTopic) {
+      _topicRepository = topicRepository;
+      _currentTopic = currentTopic;
+    }
+
+    /*==========================================================================================================================
+    | TOPIC REPOSITORY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a reference to the Topic Repository in order to gain arbitrary access to the entire topic graph.
+    /// </summary>
+    /// <returns>The TopicRepository associated with the controller.</returns>
+    protected ITopicRepository TopicRepository {
+    	get {
+        return _topicRepository;
+      }
+    }
+
+    /*==========================================================================================================================
+    | CURRENT TOPIC
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a reference to the current topic associated with the request.
+    /// </summary>
+    /// <returns>The Topic associated with the current request.</returns>
+    protected Topic CurrentTopic {
+      get {
+        return _currentTopic;
+      }
+    }
+
+    /*==========================================================================================================================
+    | [GET] INDEX
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Present an editor view bound to a specific topic.
+    /// </summary>
+    public ActionResult Index() {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(EditorBindingModel) {
+      return Index();
+    }
+
+  } //Class
+
+} //Namespace
---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/EditorAreaRegistration.cs
@@ -0,0 +1,55 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Web.Mvc;
+
+namespace Ignia.Topics.Editor.Mvc.Areas.Editor {
+
+  /*============================================================================================================================
+  | CLASS: EDITOR AREA REGISTRATION
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Registers configuration objects associated with the editor area, including route configuration.
+  /// </summary>
+  public class EditorAreaRegistration : AreaRegistration {
+
+    /*==========================================================================================================================
+    | AREA NAME
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Establishes the friendly name for the area.
+    /// </summary>
+    /// <returns>The name of the area.</returns>
+    public override string AreaName {
+      get {
+        return "Editor";
+      }
+    }
+
+    /*==========================================================================================================================
+    | METHOD: REGISTER AREA
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provided a <see cref="AreaRegistrationContext"/>, which allows area-specific configuration of e.g. routes.
+    /// </summary>
+    /// <param name="context">
+    ///   The <see cref="AreaRegistrationContext"/>, typically passed from the <see cref="System.Web.HttpApplication"/> class.
+    /// </param>
+    public override void RegisterArea(AreaRegistrationContext context) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Handle OnTopic Web namespace
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      context.Routes.MapRoute(
+        name: "TopicEditor",
+        url: "Edit/{*path}",
+        defaults: new { controller = "Editor", action = "Index", id = UrlParameter.Optional }
+      );
+
+    }
+
+  } //Class
+
+} //Namespace
