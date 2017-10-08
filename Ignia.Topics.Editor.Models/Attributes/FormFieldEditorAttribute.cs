c6ab6f9f0f62798298c6e877630dc85ea36d9db1
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-10-07 17:01:58 -0700

    Scaffolded placeholder `EditorAttributes`
    Specifically, introduced `BooleanEditorAttribute` and `FormFieldEditorAttribute`.

---
+++ Ignia.Topics.Editor.Models/Attributes/FormFieldEditorAttribute.cs
@@ -0,0 +1,50 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+
+namespace Ignia.Topics.Editor.Models.Attributes {
+
+  /*============================================================================================================================
+  | CLASS: BOOLEAN EDITOR ATTRIBUTE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents an instance of a boolean attribute in the Topic Editor.
+  /// </summary>
+  /// <remarks>
+  /// </remarks>
+  public class BooleanEditorAttribute : EditorAttribute {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private                     string                          _key                            = null;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="EditorAttribute"/> class, using the specified key/value pair.
+    /// </summary>
+    public BooleanEditorAttribute() : base() {
+    }
+
+    /*==========================================================================================================================
+    | GET VALUE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Retrieves the value associated with the attribute.
+    /// </summary>
+    /// <remarks>
+    ///   The CkEditor that Ignia uses for the frontend will occasionally inject extraneous line breaks. Prior to saving, this
+    ///   strips any extra line breaks from the body.
+    /// </remarks>
+    public override string GetValue() {
+      return Value.Equals("True")? "1" : "0";
+    }
+
+  } //Class
+
+} //Namespace
