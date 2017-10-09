b2ed5648c4384ce56965b4aed28918b1a2d89ca3
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-10-09 11:14:09 -0700

    Updated properties

--- Ignia.Topics.Editor.Mvc/Properties/AssemblyInfo.cs
+++ Ignia.Topics.Editor.Mvc/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ignia.Topics.Editor.Mvc")]
+[assembly: AssemblyTitle("Ignia Topics Editor (MVC edition)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ignia, LLC")]
 [assembly: AssemblyProduct("Ignia.Topics.Editor.Mvc")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
