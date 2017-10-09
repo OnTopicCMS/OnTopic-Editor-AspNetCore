788afff795e10c4fc5de41e4a9471d2bfbb7f0d8
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-10-09 14:25:38 -0700

    Added `Type` property
    Default to the current type name (includes derivatives) if not explicitly set. Used by `CreateModel` to determine current type.

--- Ignia.Topics.Editor.Models/Attributes/EditorAttributeModelBinder.cs
+++ Ignia.Topics.Editor.Models/Attributes/EditorAttributeModelBinder.cs
@@ -36,8 +36,8 @@ namespace Ignia.Topics.Editor.Models.Attributes {
     ///   convention, when it attempts to bind a model with a corresponding name.
     /// </remarks>
     protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType) {
-      var typeValue = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + ".ModelType");
       var type = Type.GetType((string)typeValue.ConvertTo(typeof(string)), true);
+      var typeValue = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + ".Type");
       var model = Activator.CreateInstance(type);
       bindingContext.ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(() => model, type);
       return model;
