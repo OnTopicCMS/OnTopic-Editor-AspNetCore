bd5aed5eaf49826831d85ec1e19f5254d6df6803
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-10-09 18:13:20 -0700

    Intoduced `AttributeViewModel`
    Which includes a `Topic`, `Attribute`, and `ITopicRepository` reference.

---
+++ Ignia.Topics.Editor.Models/AttributeViewModel.cs
@@ -0,0 +1,75 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using Ignia.Topics.Collections;
+using Ignia.Topics.Repositories;
+
+namespace Ignia.Topics.Editor.Models {
+
+  /*============================================================================================================================
+  | CLASS: EDITOR VIEW MODEL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents a model for an individual <see cref="Ignia.Topics.Attribute"/>, its values, and dependencies.
+  /// </summary>
+  /// <remarks>
+  /// </remarks>
+  public class AttributeViewModel {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="AttributeValue"/> class, using the specified key/value pair.
+    /// </summary>
+    public AttributeViewModel(Attribute attribute, Topic topic, ITopicRepository topicRepository) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set properties
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Attribute = attribute;
+      Topic = topic;
+      TopicRepository = topicRepository;
+
+    }
+
+    /*==========================================================================================================================
+    | ATTRIBUTE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides the global definition for the attribute, as defined on the corresponding <see cref="ContentType"/>.
+    /// </summary>
+    public Attribute Attribute {
+      get;
+    }
+
+    /*==========================================================================================================================
+    | TOPIC
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a reference to the current <see cref="AttributeValue"/> instance of the attribute.
+    /// </summary>
+    public Topic Topic {
+      get;
+    }
+
+    /*==========================================================================================================================
+    | TOPIC REPOSITORY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a reference to the <see cref="ITopicRepository"/> for dynamic access to the entire topic graph.
+    /// </summary>
+    public ITopicRepository TopicRepository {
+      get;
+    }
+
+  } //Class
+
+} //Namespace
