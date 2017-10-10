45496a2d6035c935454e3a789581764c925e44b3
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-10-09 18:53:50 -0700

    Included necessary `using` statement

--- Ignia.Topics.Editor.Models/Attributes/EditorAttribute.cs
+++ Ignia.Topics.Editor.Models/Attributes/EditorAttribute.cs
@@ -4,6 +4,7 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using System;
+using System.Web.Mvc;
 
 namespace Ignia.Topics.Editor.Models.Attributes {
 
