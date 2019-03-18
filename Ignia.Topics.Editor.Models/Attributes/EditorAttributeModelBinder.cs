cfad4026892a477b262bff3c61463ae092381a5d
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-18 04:55:02 -0700

    Migrated to new `IModelBinder` interface
    The legacy `DefaultModelBinder` base class—and its `CreateModel()` method—is no longer supported. That said, a very similar `IModelBinder` interface—with a `BindModelAsync()` method—is supported. This update migrates to the new *ASP.NET Core* format. Note that this may require additional annotations or an `IModelBinderProvider` to ensure that this is honored during the binding process; that will be determined as part of integration testing.

--- Ignia.Topics.Editor.Models/Attributes/EditorAttributeModelBinder.cs
+++ Ignia.Topics.Editor.Models/Attributes/EditorAttributeModelBinder.cs
@@ -4,6 +4,7 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Ignia.Topics.Editor.Models.Attributes {
@@ -16,10 +17,18 @@ namespace Ignia.Topics.Editor.Models.Attributes {
   ///   This is necessary to retain strongly typed instances in <see cref="EditorBindingModel.Attributes"/>, which otherwise
   ///   exposes a collection of <see cref="EditorAttribute"/> instances.
   /// </summary>
-  public class EditorAttributeModelBinder : DefaultModelBinder {
+  public class EditorAttributeModelBinder : IModelBinder {
 
     /*==========================================================================================================================
-    | OVERRIDE: CREATE MODEL
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref="EditorAttributeModelBinder"/>.
+    /// </summary>
+    public EditorAttributeModelBinder() { }
+
+    /*==========================================================================================================================
+    | OVERRIDE: BIND MODEL (ASYNC)
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Binds the incoming post to the <see cref="EditorBindingModel"/>.
@@ -28,12 +37,13 @@ namespace Ignia.Topics.Editor.Models.Attributes {
     ///   The <see cref="CreateModel(ControllerContext, ModelBindingContext, Type)"/> method is called by the MVC framework, via
     ///   convention, when it attempts to bind a model with a corresponding name.
     /// </remarks>
-    protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType) {
-      var typeValue = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + ".Type");
-      var type = Type.GetType("Ignia.Topics.Editor.Models.Attributes." + (string)typeValue.ConvertTo(typeof(string)) + "EditorAttribute", true);
+    public Task BindModelAsync(ModelBindingContext bindingContext) {
+      var modelName = bindingContext.ModelName;
+      var typeValue = bindingContext.ValueProvider.GetValue(modelName + ".Type").FirstValue;
+      var type = Type.GetType("Ignia.Topics.Editor.Models.Attributes." + typeValue.ToString() + "EditorAttribute", true);
       var model = Activator.CreateInstance(type);
-      bindingContext.ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(() => model, type);
-      return model;
+      bindingContext.Result = ModelBindingResult.Success(model);
+      return Task.CompletedTask;
     }
 
   } // Class
