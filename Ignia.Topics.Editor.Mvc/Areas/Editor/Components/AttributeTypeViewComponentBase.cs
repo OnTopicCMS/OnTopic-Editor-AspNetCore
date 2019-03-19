e100b17dc29e364be230448a5e315553b67cd51b
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-19 01:31:58 -0700

    Introduced `AttributeTypeViewComponentBase`
    The `AttributeTypeViewComponentBase` will permit the migration from `@Html.EditorFor()` templates to the more sophisticated view models, which allow multiple parameters (if needed) and, most importantly, the injection of dependencies. This class only provides an abstract base—actual concrete implementations will also be required.

---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/AttributeTypeViewComponentBase.cs
@@ -0,0 +1,86 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ignia.Topics.Mapping;
+using Ignia.Topics.Models;
+using Ignia.Topics.AspNetCore.Mvc.Models;
+using Ignia.Topics.Editor.Models;
+
+namespace Ignia.Topics.AspNetCore.Mvc.Components {
+
+  /*============================================================================================================================
+  | CLASS: ATTRIBUTE TYPE (VIEW COMPONENT)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Defines a foundation for custom <see cref="ViewComponent"/> implementations that is based on an <see
+  ///   cref="AttributeDescriptor"/> and a current <see cref="Topic"/> reference.
+  ///   model.
+  /// </summary>
+  /// <remarks>
+  ///   This class is intended to provide a foundation for concrete implementations. It is not a fully formed implementation
+  ///   itself. As a result, it is marked as <c>abstract</c>.
+  /// </remarks>
+  public abstract class AttributeTypeViewComponentBase : ViewComponent {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private readonly            ITopicRoutingService            _topicRoutingService            = null;
+    private                     Topic                           _currentTopic                   = null;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref="AttributeTypeViewComponentBase"/> with necessary dependencies.
+    /// </summary>
+    /// <returns>A topic <see cref="NavigationTopicViewComponentBase{T}"/>.</returns>
+    protected AttributeTypeViewComponentBase(
+      ITopicRoutingService topicRoutingService
+    ) {
+      _topicRoutingService = topicRoutingService;
+    }
+
+    /*==========================================================================================================================
+    | CURRENT TOPIC
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a reference to the current topic associated with the request.
+    /// </summary>
+    /// <returns>The Topic associated with the current request.</returns>
+    protected Topic CurrentTopic {
+      get {
+        if (_currentTopic == null) {
+          _currentTopic = _topicRoutingService.GetCurrentTopic();
+        }
+        return _currentTopic;
+      }
+    }
+
+    /*==========================================================================================================================
+    | METHOD: GET ATTRIBUTE VIEW MODEL
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given an <see cref="AttributeDescriptorTopicViewModel"/> and, optionally, a target <see cref="AttributeViewModel"/>,
+    ///   ensures that the properties of the <see cref="AttributeViewModel"/> are properly set.
+    /// </summary>
+    /// <returns>The Topic associated with the current request.</returns>
+    public AttributeViewModel GetAttributeViewModel(
+      AttributeDescriptorTopicViewModel attribute,
+      AttributeViewModel viewModel = null
+    ) {
+
+      viewModel = viewModel ?? new AttributeViewModel(attribute);
+      viewModel.InheritedValue = CurrentTopic.Parent.Attributes.GetValue(attribute.Key, true);
+      viewModel.Value = CurrentTopic.Attributes.GetValue(attribute.Key, attribute.DefaultValue, false, false);
+
+      return viewModel;
+
+    }
+
+  } // Class
+} // Namespace
