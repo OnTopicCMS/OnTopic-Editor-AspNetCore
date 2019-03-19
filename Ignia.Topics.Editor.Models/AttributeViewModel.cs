05aa99ca24f78fb2d7fb5687cb77353aeb6b90f1
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-19 01:35:39 -0700

    Updated `AttributeViewModel` to work with `AttributeTypeViewComponent`
    With the migration from `@Html.EditorFor()` to `AttributeTypeViewComponentBase`, the needs of the `AttributeViewModel` have changed. It now simply needs to reference the `AttributeDescriptorTopicViewModel` as well as provide a reference for the `InheritedValue` and `Value` properties. It doesn't need to relay the original `AttributeDescriptor` entity, nor the entirety of the current `Topic` entity. And it certainly doesn't need access to services, such as the `ITopicRepository`.

--- Ignia.Topics.Editor.Models/AttributeViewModel.cs
+++ Ignia.Topics.Editor.Models/AttributeViewModel.cs
@@ -4,16 +4,15 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using Ignia.Topics.Metadata;
-using Ignia.Topics.Repositories;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace Ignia.Topics.Editor.Models {
 
   /*============================================================================================================================
-  | CLASS: EDITOR VIEW MODEL
+  | CLASS: ATTRIBUTE VIEW MODEL
   \---------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
-  ///   Represents a model for an individual <see cref="Ignia.Topics.Attribute"/>, its values, and dependencies.
+  ///   Provides access to both a <see cref="AttributeDescriptorTopicViewModel"/> as well as the instance values for that
+  ///   attribute from the currently selected <see cref="Topic"/>.
   /// </summary>
   public class AttributeViewModel {
 
@@ -21,50 +20,30 @@ namespace Ignia.Topics.Editor.Models {
     | CONSTRUCTOR
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
-    ///   Initializes a new instance of the <see cref="AttributeValue"/> class, using the specified key/value pair.
+    ///   Initializes a new instance of the <see cref="AttributeViewModel"/> class.
     /// </summary>
     public AttributeViewModel(
-      AttributeDescriptor attributeDescriptor,
-      Topic topic, ITopicRepository topicRepository,
-      ViewDataDictionary viewData
+      AttributeDescriptorTopicViewModel attributeDescriptor,
+      string value = null,
+      string inheritedValue = null
     ) {
 
       /*------------------------------------------------------------------------------------------------------------------------
       | Set properties
       \-----------------------------------------------------------------------------------------------------------------------*/
-      Definition                = attributeDescriptor;
-      Topic                     = topic;
-      TopicRepository           = topicRepository;
-      ViewData                  = viewData;
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Set values
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      Key                       = attributeDescriptor.Key;
-      Value                     = topic.Attributes.GetValue(attributeDescriptor.Key, null, false, false);
-      InheritedValue            = topic.Attributes.GetValue(attributeDescriptor.Key);
+      AttributeDescriptor       = attributeDescriptor;
+      Value                     = value;
+      InheritedValue            = inheritedValue;
 
     }
 
     /*==========================================================================================================================
-    | ATTRIBUTE
+    | PROPERTY: ATTRIBUTE DESCRIPTOR
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Provides the global definition for the attribute, as defined on the corresponding <see cref="ContentType"/>.
     /// </summary>
-    public AttributeDescriptor Definition {
-      get;
-    }
-
-    /*==========================================================================================================================
-    | KEY
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Provides the current key, as defined on the <see cref="AttributeValue"/> instance.
-    /// </summary>
-    public string Key {
-      get;
-    }
+    public AttributeDescriptorTopicViewModel AttributeDescriptor { get; set; }
 
     /*==========================================================================================================================
     | VALUE
@@ -72,19 +51,7 @@ namespace Ignia.Topics.Editor.Models {
     /// <summary>
     ///   Provides the current value, as defined on the <see cref="AttributeValue"/> instance.
     /// </summary>
-    public string Value {
-      get;
-    }
-
-    /*==========================================================================================================================
-    | TOPIC
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Provides a reference to the current <see cref="Topic"/>.
-    /// </summary>
-    public Topic Topic {
-      get;
-    }
+    public string Value { get; set; }
 
     /*==========================================================================================================================
     | INHERITED VALUE
@@ -96,43 +63,7 @@ namespace Ignia.Topics.Editor.Models {
     ///   If the <see cref="Value"/> is set, then the <see cref="InhertedValue"/> will always be equal to the
     ///   <see cref="Value"/>.
     /// </remarks>
-    public string InheritedValue {
-      get;
-    }
-
-    /*==========================================================================================================================
-    | TOPIC REPOSITORY
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Provides a reference to the <see cref="ITopicRepository"/> for dynamic access to the entire topic graph.
-    /// </summary>
-    public ITopicRepository TopicRepository {
-      get;
-    }
-
-    /*==========================================================================================================================
-    | VIEW DATA
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Provides a reference to the <see cref="ViewDataDictionary"/> for the request.
-    /// </summary>
-    private ViewDataDictionary ViewData {
-      get;
-    }
-
-    /*==========================================================================================================================
-    | PROPERTY: GET CONFIGURATION VALUE
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Retrieves a configuration value from the <see cref="Configuration"/> dictionary; if the value doesn't exist, then
-    ///   optionally returns a default value.
-    /// </summary>
-    public string GetConfigurationValue(string key, string defaultValue = null) {
-      if (ViewData != null && ViewData.ContainsKey(key) && ViewData[key] != null) {
-        return ViewData[key].ToString();
-      }
-      return Definition.GetConfigurationValue(key, defaultValue);
-    }
+    public string InheritedValue { get; set; }
 
   } // Class
 
