284a7cc4dcf812986b02d31dbd74df5162e17046
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-19 02:22:27 -0700

    Introduced `TopicListViewComponent`
    This is a bare bones placeholder for the view component. The actual presentation behavior will be introduced later.

--- Ignia.Topics.Editor.Mvc/SampleActivator.cs
+++ Ignia.Topics.Editor.Mvc/SampleActivator.cs
@@ -169,6 +169,9 @@ namespace OnTopicTest {
       if (type == typeof(TokenizedTopicListViewComponent)) {
         return new TokenizedTopicListViewComponent(mvcTopicRoutingService);
       }
+      if (type == typeof(TopicListViewComponent)) {
+        return new TopicListViewComponent(mvcTopicRoutingService);
+      }
       else {
         throw new Exception($"Unknown view component {type.Name}");
       }
