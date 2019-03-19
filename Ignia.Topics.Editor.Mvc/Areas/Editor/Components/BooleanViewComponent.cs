f6098648b89f3c30b243e7c80b969666186d2e2b
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-19 01:53:16 -0700

    Introduced `BooleanViewComponent`

---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/BooleanViewComponent.cs
@@ -0,0 +1,39 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Threading.Tasks;
+using Ignia.Topics.Editor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ignia.Topics.AspNetCore.Mvc.Components {
+
+  /*============================================================================================================================
+  | CLASS: BOOLEAN (VIEW COMPONENT)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Delivers a view model for a boolean attribute type.
+  /// </summary>
+  public class BooleanViewComponent: AttributeTypeViewComponentBase {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref="LastModifiedViewComponent"/> with necessary dependencies.
+    /// </summary>
+    /// <returns>A topic <see cref="NavigationTopicViewComponentBase{T}"/>.</returns>
+    public BooleanViewComponent(ITopicRoutingService topicRoutingService) : base(topicRoutingService) { }
+
+    /*==========================================================================================================================
+    | METHOD: INVOKE (ASYNC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Assembles the view model for the <see cref="LastModifiedViewComponent"/>.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync(AttributeDescriptorTopicViewModel attribute, string Id) =>
+      View(GetAttributeViewModel(attribute));
+
+  } // Class
+} // Namespace
--- Ignia.Topics.Editor.Mvc/SampleActivator.cs
+++ Ignia.Topics.Editor.Mvc/SampleActivator.cs
@@ -136,6 +136,9 @@ namespace OnTopicTest {
       if (type == typeof(LastModifiedViewComponent)) {
         return new LastModifiedViewComponent(mvcTopicRoutingService);
       }
+      if (type == typeof(BooleanViewComponent)) {
+        return new BooleanViewComponent(mvcTopicRoutingService);
+      }
       else {
         throw new Exception($"Unknown view component {type.Name}");
       }
