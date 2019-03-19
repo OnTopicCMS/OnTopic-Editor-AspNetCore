9567c15df68cdba53c18268b0497e4ac43fe3da0
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-19 02:18:22 -0700

    Introduced `TokenizedTopicListViewComponent`
    This is a bare bones placeholder for the view component. The actual presentation behavior will be introduced later.

---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/TokenizedTopicListViewComponent.cs
@@ -0,0 +1,38 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Threading.Tasks;
+using Ignia.Topics.Editor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ignia.Topics.AspNetCore.Mvc.Components {
+
+  /*============================================================================================================================
+  | CLASS: TOKENIZED TOPIC LIST (VIEW COMPONENT)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Delivers a view model for a tokenized topic list attribute type.
+  /// </summary>
+  public class TokenizedTopicListViewComponent : AttributeTypeViewComponentBase {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref="TokenizedTopicListViewComponent"/> with necessary dependencies.
+    /// </summary>
+    public TokenizedTopicListViewComponent(ITopicRoutingService topicRoutingService) : base(topicRoutingService) { }
+
+    /*==========================================================================================================================
+    | METHOD: INVOKE (ASYNC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Assembles the view model for the <see cref="TokenizedTopicListViewComponent"/>.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync(AttributeDescriptorTopicViewModel attribute, string Id) =>
+      View(GetAttributeViewModel(attribute));
+
+  } // Class
+} // Namespace
--- Ignia.Topics.Editor.Mvc/SampleActivator.cs
+++ Ignia.Topics.Editor.Mvc/SampleActivator.cs
@@ -166,6 +166,9 @@ namespace OnTopicTest {
       if (type == typeof(RelationshipsViewComponent)) {
         return new RelationshipsViewComponent(mvcTopicRoutingService);
       }
+      if (type == typeof(TokenizedTopicListViewComponent)) {
+        return new TokenizedTopicListViewComponent(mvcTopicRoutingService);
+      }
       else {
         throw new Exception($"Unknown view component {type.Name}");
       }
