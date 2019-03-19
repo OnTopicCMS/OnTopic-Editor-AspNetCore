0d097098d7f4d29219f8ece5797f6e4d8c0f19ca
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-19 02:01:36 -0700

    Introduced `FileViewComponent`
    This is a bare bones placeholder for the view component. The actual presentation behavior will be introduced later.

---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/FileViewComponent.cs
@@ -0,0 +1,38 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Threading.Tasks;
+using Ignia.Topics.Editor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ignia.Topics.AspNetCore.Mvc.Components {
+
+  /*============================================================================================================================
+  | CLASS: FILE (VIEW COMPONENT)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Delivers a view model for a file attribute type.
+  /// </summary>
+  public class FileViewComponent: AttributeTypeViewComponentBase {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref="FileViewComponent"/> with necessary dependencies.
+    /// </summary>
+    public FileViewComponent(ITopicRoutingService topicRoutingService) : base(topicRoutingService) { }
+
+    /*==========================================================================================================================
+    | METHOD: INVOKE (ASYNC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Assembles the view model for the <see cref="FileViewComponent"/>.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync(AttributeDescriptorTopicViewModel attribute, string Id) =>
+      View(GetAttributeViewModel(attribute));
+
+  } // Class
+} // Namespace
--- Ignia.Topics.Editor.Mvc/SampleActivator.cs
+++ Ignia.Topics.Editor.Mvc/SampleActivator.cs
@@ -145,6 +145,9 @@ namespace OnTopicTest {
       if (type == typeof(DisplayOptionsViewComponent)) {
         return new DisplayOptionsViewComponent(mvcTopicRoutingService);
       }
+      if (type == typeof(FileViewComponent)) {
+        return new FileViewComponent(mvcTopicRoutingService);
+      }
       else {
         throw new Exception($"Unknown view component {type.Name}");
       }
