a271726ecbcac055a63250efe2e604624f67a390
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-19 01:33:09 -0700

    Introduced `LastModifiedViewComponent` as example
    We'll need concrete versions for _every_ attribute type, but for now am introducing the `LastModifiedViewComponent` as a proof-of-concept.

---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/LastModifiedViewComponent.cs
@@ -0,0 +1,51 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Threading.Tasks;
+using Ignia.Topics.Editor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ignia.Topics.AspNetCore.Mvc.Components {
+
+  /*============================================================================================================================
+  | CLASS: LAST MODIFIED (VIEW COMPONENT)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Delivers a view model for a last modified attribute type.
+  /// </summary>
+  public class LastModifiedViewComponent: AttributeTypeViewComponentBase {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref="LastModifiedViewComponent"/> with necessary dependencies.
+    /// </summary>
+    /// <returns>A topic <see cref="NavigationTopicViewComponentBase{T}"/>.</returns>
+    public LastModifiedViewComponent(ITopicRoutingService topicRoutingService) : base(topicRoutingService) { }
+
+    /*==========================================================================================================================
+    | METHOD: INVOKE (ASYNC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Assembles the view model for the <see cref="LastModifiedViewComponent"/>.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync(AttributeDescriptorTopicViewModel attribute, string Id) {
+      var viewModel = GetAttributeViewModel(attribute);
+      if (viewModel.Value == null) {
+        if (CurrentTopic.LastModified != null && CurrentTopic.LastModified != DateTime.MinValue) {
+          viewModel.Value = CurrentTopic.LastModified.ToString();
+        }
+        else {
+          viewModel.Value = DateTime.Now.ToString();
+        }
+      }
+      return View(viewModel);
+    }
+
+
+  } // Class
+} // Namespace
