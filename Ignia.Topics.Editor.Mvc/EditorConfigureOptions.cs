2b84fa9e4a0f69c5a490071e7b565415e0969b85
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-21 01:42:52 -0700

    Introduced `EditorConfigureOptions` and related configuration apparatus
    The `EditorConfigureOptions` configures a file system provider that references embedded resources. The `EditorServiceCollectionExtensions` makes it easy to register this as part of the application startup process. And, indeed, that extension method is then added to the `Startup` class in the `Host` project in order to do just that.

---
+++ Ignia.Topics.Editor.Mvc/EditorConfigureOptions.cs
@@ -0,0 +1,80 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using Ignia.Topics.Internal.Diagnostics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+
+namespace Ignia.Topics.Editor.Mvc {
+
+  /*============================================================================================================================
+  | CLASS: EDITOR (CONFIGURE OPTIONS)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Used to configure an MVC application to use embedded resources from this Razor Class Library (RCL).
+  /// </summary>
+  public class EditorConfigureOptions : IPostConfigureOptions<StaticFileOptions> {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private readonly            IHostingEnvironment             _environment                    = null;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref="EditorConfigureOptions"/> with necessary dependencies.
+    /// </summary>
+    /// <returns>A topic controller for loading OnTopic views.</returns>
+    public EditorConfigureOptions(IHostingEnvironment environment) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate input
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Contract.Requires(environment != null, "A concrete implementation of an IHostingEnvironment is required.");
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set values locally
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      _environment = environment;
+
+    }
+
+    /*==========================================================================================================================
+    | METHOD: POST CONFIGURE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Extends the existing configuration with options specific to the editor.
+    /// </summary>
+    public void PostConfigure(string name, StaticFileOptions options) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate dependencies
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (options.FileProvider == null && _environment.WebRootFileProvider == null) {
+        throw new InvalidOperationException("Missing FileProvider.");
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Ensure basic options are configured
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      options.ContentTypeProvider = options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
+      options.FileProvider = options.FileProvider ?? _environment.WebRootFileProvider;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add provider for embedded resources
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var fileProvider = new ManifestEmbeddedFileProvider(this.GetType().Assembly, "wwwroot");
+      options.FileProvider = new CompositeFileProvider(options.FileProvider, fileProvider);
+
+    }
+
+  } // Class
+} // Namespace
---
+++ Ignia.Topics.Editor.Mvc/EditorServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ignia.Topics.Editor.Mvc {
+
+  /*============================================================================================================================
+  | CLASS: EDITOR SERVICE COLLECTION (EXTENSIONS)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Extension method to simplify configuration of the editor in an application. Automatically sets up e.g. <see
+  ///   cref="EditorConfigureOptions"/>.
+  /// </summary>
+  public static class EditorServiceCollectionExtensions {
+
+    /*==========================================================================================================================
+    | METHOD: ADD TOPIC EDITOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Ensures that dependencies of the topic editor are configured.
+    /// </summary>
+    public static void AddTopicEditor(this IServiceCollection services) {
+      services.ConfigureOptions(typeof(EditorConfigureOptions));
+    }
+
+  } // Class
+} // Namespace
