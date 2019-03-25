aa812b20b55bc41c8ccb170e1c1007d75a6e77b1
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-03-24 21:27:47 -0700

    Introduced `TopicLookupAttributeViewModel`
    The `TopicLookupViewComponent` requires a custom view model in order to relay the select box options. This can be accomplished by simply adding a `List<SelectListItem>` to a derivation of `AttributeViewModel`. That said, the `SelectListItem` class is in an assembly that drags with it the majority of *ASP.NET Core*—none of which the rest of the models classes need, or should need. As a result, it is being created in the `Mvc` project. This is a bit inconsistent, but is preferrable to the `Models` project needing to take on a lot of new dependencies for one class.

---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Models/TopicLookupAttributeViewModel.cs
@@ -0,0 +1,52 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Collections.Generic;
+using Ignia.Topics.Editor.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ignia.Topics.Editor.Mvc.Models {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC LOOKUP (ATTRIBUTE VIEW MODEL)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Extends the <see cref="AttributeViewModel"/> to include properties that are specific to the topic lookup view component.
+  /// </summary>
+  public class TopicLookupAttributeViewModel: AttributeViewModel {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="TopicLookupAttributeViewModel"/> class.
+    /// </summary>
+    public TopicLookupAttributeViewModel(
+      AttributeDescriptorTopicViewModel attributeDescriptor,
+      string value = null,
+      string inheritedValue = null
+    ) : base(
+      attributeDescriptor,
+      value,
+      inheritedValue
+    ) {}
+
+    /*==========================================================================================================================
+    | OPTIONS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a list of key/value pairs associated with the topic lookup.
+    /// </summary>
+    /// <remarks>
+    ///   The key represents the value that will be persisted to the <see cref="Topic.Attributes"/> collection. The value
+    ///   represents the label as it will be displayed in the interface. For instance, for a <see
+    ///   cref="TopicLookupAttributeViewModel"/> representing countries, <c>US</c> might by the key associated with a <c>United
+    ///   States</c> value.
+    /// </remarks>
+    public List<SelectListItem> Options { get; } = new List<SelectListItem>();
+
+  } // Class
+
+} // Namespace
