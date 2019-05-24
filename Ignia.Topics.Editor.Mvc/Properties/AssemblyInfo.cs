052570eafa545f5989f411cad42858c76d2df9d8
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-05-23 22:28:48 -0700

    Removed redundant properties
    In the .NET Core project format, project properties are now in the `csproj` file, not in the `AssemblyInfo.cs` file. For some reason, this worked fine up until upgrading to the latest version of the `Ignia.Topics.dll`. With the latest version, however, the `dotnet` compiler started tripping on the duplicate properties. Given that, removed all duplicates, both allowing the editor project to compile, and making it consistent with other .NET Core projects.

--- Ignia.Topics.Editor.Mvc/Properties/AssemblyInfo.cs
+++ Ignia.Topics.Editor.Mvc/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Ignia Topics Editor (MVC edition)")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ignia, LLC")]
-[assembly: AssemblyProduct("Ignia.Topics.Editor.Mvc")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -31,5 +19,3 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
