8f11da36c560a4c7fb474b567ee403f23f24683a
Jeremy Caney <jeremy.caney@ignia.com>
2019-10-20 19:12:02 -0700

    Introduced `AttributeViewModel<T>`
    The `AttributeViewModel<T>` accepts an options class as the generic type, which can be `DefaultOptions` or any derivative. This allows it to introduce a strongly type `Options` property. Once implemented, this will give all views access to the `Options` used to initialize the attribute view component. This prevents the need to duplicate these properties on the `AttributeViewModel` (or derivative) in the case that they need to be relayed directly to the view.

---
+++ Ignia.Topics.Editor.Models/AttributeViewModel{T}.cs
@@ -0,0 +1,49 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using Ignia.Topics.Editor.Models.Components.Options;
+using Ignia.Topics.Metadata;
+
+namespace Ignia.Topics.Editor.Models {
+
+  /*============================================================================================================================
+  | CLASS: ATTRIBUTE VIEW MODEL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides access to both a <see cref="AttributeDescriptorTopicViewModel"/> as well as the instance values for that
+  ///   attribute from the currently selected <see cref="Topic"/>.
+  /// </summary>
+  public class AttributeViewModel<T>: AttributeViewModel where T: DefaultOptions {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="AttributeViewModel"/> class.
+    /// </summary>
+    public AttributeViewModel(
+      AttributeDescriptorTopicViewModel attributeDescriptor,
+      T options,
+      string value = null,
+      string inheritedValue = null
+    ): base(
+      attributeDescriptor,
+      value,
+      inheritedValue
+    ) {
+      Options = options;
+    }
+
+    /*==========================================================================================================================
+    | OPTIONS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Exposes the options associated with the specific attribute view model.
+    /// </summary>
+    public T Options { get; set; }
+
+  } // Class
+
+} // Namespace
