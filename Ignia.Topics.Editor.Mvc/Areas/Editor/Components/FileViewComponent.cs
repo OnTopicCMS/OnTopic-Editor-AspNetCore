bfde9f96900ceccec112f7e2358bc09fcad5b959
Jeremy Caney <jeremy.caney@ignia.com>
2019-10-21 19:23:39 -0700

    Established deprecated pass-through for `FileViewComponent`
    In a previous commit (#), we renamed `FileViewComponent` to `FileListViewComponent`. Legacy databases, however, may still be calling `FileViewComponent`. These should be updated. To simplify this transition, however, an instance of `FileViewComponent` was reintroduced, which simply subclasses `FileListViewComponent`, thus ensuring it isn't duplicating any logic. The class is marked as `[Obsolete()]` since it isn't intended to be used—though, users will only discover this in their composition root, since no one is instantiating this directly.

---
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/FileViewComponent.cs
@@ -0,0 +1,43 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+
+using Ignia.Topics.Editor.Models.Components.Options;
+using Ignia.Topics.Repositories;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace Ignia.Topics.Editor.Mvc.Components {
+
+  /*============================================================================================================================
+  | CLASS: FILE (VIEW COMPONENT)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   A simple pass-through to <see cref="FileListViewComponent"/>, intended exclusively for backward compatibility.
+  /// </summary>
+  /// <remarks>
+  ///   The <see cref="FileViewComponent"/> was recently renamed to <see cref="FileListViewComponent"/>. Legacy databases,
+  ///   however, may still be using the previous name (i.e., <c>File</c>, not <c>FileList</c>). As such, this pass-through is
+  ///   being maintained for a short period to aid in the migration.
+  /// </remarks>
+  [Obsolete("The 'FileViewComponent' is deprecated. Please use 'FileListViewComponent' instead.", false)]
+  public class FileViewComponent : FileListViewComponent {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref="HtmlViewComponent"/> with necessary dependencies.
+    /// </summary>
+    public FileViewComponent(
+      ITopicRoutingService topicRoutingService,
+      IWebHostEnvironment webHostEnvironment
+    ) : base(
+      topicRoutingService,
+      webHostEnvironment
+    ) { }
+
+  } // Class
+} // Namespace
