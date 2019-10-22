f7de75ccb6933126bc175a7f428f76b471d4d11d
Jeremy Caney <jeremy.caney@ignia.com>
2019-10-22 01:49:04 -0700

    Introduced initial version of `TopicPointerViewComponent`
    The `TopicPointerOptions` establishes the properties that can be set via `DefaultOptions`; the view relays the properties down to the underlying `TokenizedTopicListViewComponent`, along with some predefined values.

---
+++ Ignia.Topics.Editor.Models/Components/Options/TopicPointerOptions.cs
@@ -0,0 +1,48 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using Ignia.Topics.Metadata;
+using System;
+
+#nullable enable
+
+namespace Ignia.Topics.Editor.Models.Components.Options {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC POINTER (OPTIONS)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Establishes options associated with the <see cref="TopicPointerViewComponent"/>.
+  /// </summary>
+  public class TopicPointerOptions: DefaultOptions {
+
+    /*==========================================================================================================================
+    | SCOPE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the scope of the topic graph within which to search for results. E.g., <c>Root:Web:Configuration</c>.
+    /// </summary>
+    public string? Scope { get; set; }
+
+    /*==========================================================================================================================
+    | RESULT LIMIT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the maximum number of <see cref="Topic"/> results to pull from the web service.
+    /// </summary>
+    public int? ResultLimit { get; set; }
+
+    /*==========================================================================================================================
+    | CONTENT TYPE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the <see cref="Topic.Key"/> of the <see cref="ContentTypeDescriptor"/> to filter results by.
+    /// </summary>
+    public string? ContentType { get; set; }
+
+  } // Class
+} // Namespace
+
+#nullable restore
--- Ignia.Topics.Editor.Mvc/Areas/Editor/Components/TopicPointerViewComponent.cs
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/TopicPointerViewComponent.cs
@@ -4,6 +4,15 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 
+using Ignia.Topics.Editor.Models;
+using Ignia.Topics.Editor.Models.Components.Options;
+using Ignia.Topics.Editor.Models.Components.ViewModels;
+using Ignia.Topics.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace Ignia.Topics.Editor.Mvc.Components {
 
   /*============================================================================================================================
@@ -12,7 +21,12 @@ namespace Ignia.Topics.Editor.Mvc.Components {
   /// <summary>
   ///   Delivers a view model for a topic pointer attribute type.
   /// </summary>
-  public class TopicPointerViewComponent : DefaultAttributeTypeViewComponent {
+  public class TopicPointerViewComponent : AttributeTypeViewComponentBase {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private readonly            ITopicRepository                _topicRepository                = null;
 
     /*==========================================================================================================================
     | CONSTRUCTOR
@@ -20,7 +34,58 @@ namespace Ignia.Topics.Editor.Mvc.Components {
     /// <summary>
     ///   Initializes a new instance of a <see cref="TopicPointerViewComponent"/> with necessary dependencies.
     /// </summary>
-    public TopicPointerViewComponent(ITopicRoutingService topicRoutingService) : base(topicRoutingService) { }
+    public TopicPointerViewComponent(
+      ITopicRoutingService topicRoutingService,
+      ITopicRepository topicRepository
+    ) : base(
+      topicRoutingService
+    ) {
+      _topicRepository = topicRepository;
+    }
+
+
+    /*==========================================================================================================================
+    | METHOD: INVOKE (ASYNC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Assembles the view model for the <see cref="TopicListViewComponent"/>.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync(
+      AttributeDescriptorTopicViewModel attribute,
+      string htmlFieldPrefix,
+      TopicPointerOptions options = null
+    ) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set HTML prefix
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      ViewData.TemplateInfo.HtmlFieldPrefix = htmlFieldPrefix;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set configuration values
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      options                   ??= new TopicPointerOptions();
+      options.Scope             ??= attribute.GetConfigurationValue(            "Scope",                "Root");
+      options.ResultLimit       ??= attribute.GetIntegerConfigurationValue(     "ResultLimit",          100);
+      options.ContentType       ??= attribute.GetConfigurationValue(            "ContentType",          null);
+
+      if (options.ContentType is null) {
+        options.ContentType = CurrentTopic.ContentType;
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var viewModel = new AttributeViewModel<TopicPointerOptions>(attribute, options);
+
+      GetAttributeViewModel(viewModel);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return view with view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return View(viewModel);
+
+    }
 
   } // Class
 } // Namespace
