809f6c7e7fce154bd79c02a1e7aa73563489d438
Jeremy Caney <jeremy.caney@ignia.com>
2019-10-21 19:30:32 -0700

    Implemented initial version of `RelationshipsViewComponent`
    This includes a new `RelationshipsOptions` class which exposes all of the `DefaultConfiguration` properties that the relationships attribute will accept. This provides a convenient way of exposing and documenting the available options in a simple format—though, of course, those options still aren't exposed to the user interface, unfortunately. This implementation includes the **ExtJS** `TreeView` control, which is configured inline, and relays the `Model.Options` to the `dataUrl` (which pulls values from the `EditorController.JsonAsync()` action). Ideally, we'd subclass the script so that common attributes aren't repeated for each relationship on a page; ultimately, however, we want to replace the **ExtJS** implementation with a lighter-weight, stand-alone tree view control. As a result, we're not worrying about optimizing this since it isn't something we want to maintain for too long.

---
+++ Ignia.Topics.Editor.Models/Components/Options/RelationshipsOptions.cs
@@ -0,0 +1,88 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using Ignia.Topics.Metadata;
+using System;
+
+#nullable enable
+
+namespace Ignia.Topics.Editor.Models.Components.Options {
+
+  /*============================================================================================================================
+  | CLASS: RELATIONSHIPS (OPTIONS)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Establishes options associated with the <see cref="RelationshipsViewComponent"/>.
+  /// </summary>
+  public class RelationshipsOptions: DefaultOptions {
+
+    /*==========================================================================================================================
+    | PROPERTY: SCOPE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets a <see cref="Topic.GetUniqueKey"/> path representing the scope of <see cref="Topic"/>s to display to the
+    ///   user. This allows relationships to be targeted to particular areas of the topic graph.
+    /// </summary>
+    public string? Scope { get; set; }
+
+    /*==========================================================================================================================
+    | PROPERTY: SHOW ROOT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given the <see cref="Scope"/>, determines whether the root node is displayed, or only the children.  The default is
+    ///   false.
+    /// </summary>
+    public bool? ShowRoot { get; set; }
+
+    /*==========================================================================================================================
+    | PROPERTY: CHECK ASCENDANTS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   When a <see cref="Topic"/> is selected as a relationship, determines if the client should automatically select all
+    ///   descendent topics. The default is false.
+    /// </summary>
+    public bool? CheckAscendants { get; set; }
+
+    /*==========================================================================================================================
+    | PROPERTY: ATTRIBUTE NAME
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Optionally defines an attribute name to filter the list of displayed <see cref="Topic"/>s by. Must be accompanied by
+    ///   a <see cref="AttributeValue"/>.
+    /// </summary>
+    /// <remarks>
+    ///   If the <see cref="AttributeName"/> and <see cref="AttributeValue"/> are defined, then any <see cref="Topic"/>s listed
+    ///   under a <see cref="Topic"/> that is excluded by the filter will <i>also</i> be excluded. As such, this option should
+    ///   be used with care.
+    /// </remarks>
+    public string? AttributeName { get; set; }
+
+    /*==========================================================================================================================
+    | PROPERTY: ATTRIBUTE VALUE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Optionally defines an attribute value to filter the list of displayed <see cref="Topic"/>s by. Must be accompanied by
+    ///   a <see cref="AttributeName"/>.
+    /// </summary>
+    /// <remarks>
+    ///   If the <see cref="AttributeName"/> and <see cref="AttributeValue"/> are defined, then any <see cref="Topic"/>s listed
+    ///   under a <see cref="Topic"/> that is excluded by the filter will <i>also</i> be excluded. As such, this option should
+    ///   be used with care.
+    /// </remarks>
+    public string? AttributeValue { get; set; }
+
+    /*==========================================================================================================================
+    | PROPERTY: NAMESPACE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Determines the name space to save the relationship under. Any topic may have multiple relationships, so the namespace
+    ///   helps distinguish them from one another.
+    /// </summary>
+    public string? Namespace { get; set; }
+
+  } // Class
+} // Namespace
+
+#nullable restore
--- Ignia.Topics.Editor.Mvc/Areas/Editor/Components/RelationshipsViewComponent.cs
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/RelationshipsViewComponent.cs
@@ -3,6 +3,7 @@
 | Client        Ignia, LLC
 | Project       Topics Library
 \=============================================================================================================================*/
+using System;
 using System.Threading.Tasks;
 using Ignia.Topics.Editor.Models;
 using Ignia.Topics.Editor.Models.Components.Options;
@@ -35,12 +36,54 @@ namespace Ignia.Topics.Editor.Mvc.Components {
     public async Task<IViewComponentResult> InvokeAsync(
       AttributeDescriptorTopicViewModel attribute,
       string htmlFieldPrefix,
-      DefaultOptions options = null
+      RelationshipsOptions options = null
     ) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set HTML prefix
+      \-----------------------------------------------------------------------------------------------------------------------*/
       ViewData.TemplateInfo.HtmlFieldPrefix = htmlFieldPrefix;
-      options ??= new DefaultOptions();
-      var viewModel = new AttributeViewModel<DefaultOptions>(attribute, options);
-      return View(GetAttributeViewModel(viewModel));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set configuration values
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      options                   ??= new RelationshipsOptions();
+      options.Scope             ??= attribute.GetConfigurationValue(            "Scope",                null);
+      options.ShowRoot          ??= attribute.GetBooleanConfigurationValue(     "ShowRoot",             false);
+      options.CheckAscendants   ??= attribute.GetBooleanConfigurationValue(     "CheckAscendants",      false);
+      options.AttributeName     ??= attribute.GetConfigurationValue(            "AttributeName",        null);
+      options.AttributeValue    ??= attribute.GetConfigurationValue(            "AttributeValue",       null);
+      options.Namespace         ??= attribute.GetConfigurationValue(            "Namespace",            "Related");
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var model = new AttributeViewModel<RelationshipsOptions>(attribute, options);
+
+      GetAttributeViewModel(model);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set model values
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      model.Value = CleanArray(model.Value);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return view with view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return View(model);
+
+    }
+
+    /*==========================================================================================================================
+    | METHOD: CLEAN ARRAY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Takes a string array, converts it to an array, strips any blank entries, and returns it to a string array.  Useful for
+    ///   dealing with potential artifacts such as empty array items introduced by JavaScript.
+    /// </summary>
+    string CleanArray(string value) {
+      if (String.IsNullOrWhiteSpace(value)) return "";
+      return String.Join(",", value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
     }
 
   } // Class
