b0e56c00e05be51f8e6f0fcb9e4849f692a26228
Jeremy Caney <jeremy.caney@ignia.com>
2019-10-22 13:38:19 -0700

    Introduced initial version of `WysiwygViewComponent`
    The `WysiwygViewComponent` includes a custom `WysiwygOptions` class for defining what properties can be set via `DefaultConfiguration`, as well as a view which initializes the **CKEditor** script.

---
+++ Ignia.Topics.Editor.Models/Components/Options/WysiwygOptions.cs
@@ -0,0 +1,56 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using Ignia.Topics.Metadata;
+using System;
+
+#nullable enable
+
+namespace Ignia.Topics.Editor.Models.Components.Options {
+
+  /*============================================================================================================================
+  | CLASS: WYSIWYG (OPTIONS)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Establishes options associated with the <see cref="BooleanViewComponent"/>.
+  /// </summary>
+  public class WysiwygOptions: DefaultOptions {
+
+
+    /*==========================================================================================================================
+    | COLUMNS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the number of columns (width) that the <see cref="WysiwygViewComponent"/> should take up. Defaults to
+    ///   <c>70</c>.
+    /// </summary>
+    public int? Columns { get; set; }
+
+    /*==========================================================================================================================
+    | ROWS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the number of rows (height) that the <see cref="WysiwygViewComponent"/> should take up. Defaults to
+    ///   <c>30</c>.
+    /// </summary>
+    public int? Rows { get; set; }
+
+    /*==========================================================================================================================
+    | HEIGHT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the number of pixels that the <see cref="WysiwygViewComponent"/> should take up. Defaults to <see
+    ///   cref="Rows"/> x 20.
+    /// </summary>
+    /// <remarks>
+    ///   If set, this value overrides <see cref="Rows"/>.
+    /// </remarks>
+    public int? Height { get; set; }
+
+
+  } // Class
+} // Namespace
+
+#nullable restore
--- Ignia.Topics.Editor.Mvc/Areas/Editor/Components/WysiwygViewComponent.cs
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/WysiwygViewComponent.cs
@@ -3,7 +3,10 @@
 | Client        Ignia, LLC
 | Project       Topics Library
 \=============================================================================================================================*/
+using Ignia.Topics.Editor.Models;
+using Ignia.Topics.Editor.Models.Components.Options;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Ignia.Topics.Editor.Mvc.Components {
 
@@ -14,7 +17,7 @@ namespace Ignia.Topics.Editor.Mvc.Components {
   ///   Delivers a view model for a WYSIWYG attribute type.
   /// </summary>
   [ViewComponent(Name = "WYSIWYG")]
-  public class WysiwygViewComponent : DefaultAttributeTypeViewComponent {
+  public class WysiwygViewComponent : AttributeTypeViewComponentBase {
 
     /*==========================================================================================================================
     | CONSTRUCTOR
@@ -23,6 +26,51 @@ namespace Ignia.Topics.Editor.Mvc.Components {
     ///   Initializes a new instance of a <see cref="WysiwygViewComponent"/> with necessary dependencies.
     /// </summary>
     public WysiwygViewComponent(ITopicRoutingService topicRoutingService) : base(topicRoutingService) { }
+
+
+    /*==========================================================================================================================
+    | METHOD: INVOKE (ASYNC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Assembles the view model for the <see cref="TopicListViewComponent"/>.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync(
+      AttributeDescriptorTopicViewModel attribute,
+      string htmlFieldPrefix,
+      WysiwygOptions options = null
+    ) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set HTML prefix
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      ViewData.TemplateInfo.HtmlFieldPrefix = htmlFieldPrefix;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set configuration values
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      options                   ??= new WysiwygOptions();
+      options.Columns           ??= attribute.GetIntegerConfigurationValue(     "Columns",              70);
+      options.Rows              ??= attribute.GetIntegerConfigurationValue(     "Rows",                 20);
+      options.Height            ??= attribute.GetIntegerConfigurationValue(     "Height",               0);
+      options.CssClass          ??= attribute.GetConfigurationValue(            "CssClass",             "FormField Field");
+
+      if (options.Height == null || options.Height == 0 && options.Rows != null) {
+        options.Height = options.Rows * 20;
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var viewModel = new AttributeViewModel<WysiwygOptions>(attribute, options);
+
+      GetAttributeViewModel(viewModel);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return view with view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return View(viewModel);
+
+    }
 
   } // Class
 } // Namespace
