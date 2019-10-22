fa128885ff589b3a203699a190756f405efb71e8
Jeremy Caney <jeremy.caney@ignia.com>
2019-10-22 01:50:21 -0700

    Introduced `FileListEditorAttribute`
    This is necessary to ensure proper and strongly typed binding of `FileListViewComponent`-related fields.

---
+++ Ignia.Topics.Editor.Models/Attributes/FileListEditorAttribute.cs
@@ -0,0 +1,37 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+
+namespace Ignia.Topics.Editor.Models.Attributes {
+
+  /*============================================================================================================================
+  | CLASS: FILE LIST EDITOR ATTRIBUTE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents an instance of a file editor attribute in the Topic Editor.
+  /// </summary>
+  public class FileListEditorAttribute : EditorAttribute {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="FileListEdutorAttribute"/> class, using the specified key/value pair.
+    /// </summary>
+    public FileListEditorAttribute() : base() {
+    }
+
+    /*==========================================================================================================================
+    | GET VALUE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Retrieves the value associated with the attribute.
+    /// </summary>
+    public override string GetValue() => Value;
+
+  } // Class
+
+} // Namespace
