09cfe47a6558d56d3b636a62fb7f5bfce0d62495
Jeremy Caney <jeremy.caney@ignia.com>
2019-11-03 13:00:42 -0800

    Migrated to a `StaticTypeLookupService`
    While the `DynamicTypeLookupService` is more convenient, it is known for introducing problems when looking up classes in external libraries, since those libraries aren't loaded in the scope of `Ignia.Topic.dll`—even if they're loaded in context of `Ignia.Topics.Editor.Mvc`. Until we find a way around this, we need to manually register these classes.

--- Ignia.Topics.Editor.Mvc/Areas/Editor/Infrastructure/AttributeBindingModelLookupService.cs
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Infrastructure/AttributeBindingModelLookupService.cs
@@ -13,25 +13,38 @@ namespace Ignia.Topics.Editor.Mvc.Infrastructure {
   | CLASS: ATTRIBUTE BINDING MODEL (LOOKUP SERVICE)
   \---------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
-  ///   The underlying <see cref="DynamicTypeLookupService"/> will search all assemblies for <see cref="Type"/>s that end with
-  ///   <c>AttributeBindingModel</c>.
+  ///   Provides a mapping between string and class names to be used when mapping <see cref="Topic"/> to a <see
+  ///   cref="AttributeBindingModel"/> or derived class.
   /// </summary>
-  public class AttributeBindingModelLookupService : DynamicTypeLookupService {
+  public class AttributeBindingModelLookupService : StaticTypeLookupService {
 
     /*==========================================================================================================================
     | CONSTRUCTOR
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
-    ///   Establishes a new instance of a <see cref="AttributeBindingModelLookupService"/>.
+    ///   Estabblishes a new instance of the <see cref="EditorBindingModelLookupService"/>.
     /// </summary>
-    public AttributeBindingModelLookupService() : base(
-      t => (
-        t.Name.EndsWith("AttributeBindingModel", StringComparison.InvariantCultureIgnoreCase) &&
-        typeof(AttributeBindingModel).IsAssignableFrom(t)
-      ),
-      typeof(object)
-    )
-    { }
+    /// <returns>A new instance of the <see cref="EditorBindingModelLookupService"/>.</returns>
+    public AttributeBindingModelLookupService() : base() {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add Editor-specific view models
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Add(typeof(BooleanAttributeBindingModel));
+      Add(typeof(DateTimeAttributeBindingModel));
+      Add(typeof(FileListAttributeBindingModel));
+      Add(typeof(FilePathAttributeBindingModel));
+      Add(typeof(HtmlAttributeBindingModel));
+      Add(typeof(LastModifiedByAttributeBindingModel));
+      Add(typeof(NestedTopicListAttributeBindingModel));
+      Add(typeof(NumberAttributeBindingModel));
+      Add(typeof(RelationshipAttributeBindingModel));
+      Add(typeof(TextAttributeBindingModel));
+      Add(typeof(TokenizedTopicListAttributeBindingModel));
+      Add(typeof(TopicListAttributeBindingModel));
+      Add(typeof(TopicReferenceAttributeBindingModel));
+
+    }
 
   } //Class
 } //Namespace
