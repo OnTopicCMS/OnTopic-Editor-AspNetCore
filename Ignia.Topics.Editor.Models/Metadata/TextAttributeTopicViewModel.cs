fc5c31389f45d6500dcd732f394f03586807790e
Jeremy Caney <jeremy.caney@ignia.com>
2019-11-03 16:35:52 -0800

    Introduced initial version of `TextViewComponent`
    Includes ability to set `MaximumLength` and `InputType` (presumably via a reference to the `Metadata:InputTypes` lookup).

--- Ignia.Topics.Editor.Models/Metadata/TextAttributeTopicViewModel.cs
+++ Ignia.Topics.Editor.Models/Metadata/TextAttributeTopicViewModel.cs
@@ -15,6 +15,23 @@ namespace Ignia.Topics.Editor.Models.Metadata {
   ///   Provides access to attributes associated with the <see cref="TextViewComponentView"/>.
   /// </summary>
   public class TextAttributeTopicViewModel: AttributeDescriptorTopicViewModel {
+
+    /*==========================================================================================================================
+    | MAXIMUM LENGTH
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the maximimum length of the field. Defaults to <c>500</c> if undefined.
+    /// </summary>
+    public int? MaximumLength { get; set; }
+
+    /*==========================================================================================================================
+    | INPUT TYPE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the HTML input type of the field. Defaults to <c>text</c> if undefined.
+    /// </summary>
+    public string? InputType { get; set; }
+
   } //Class
 } //Namespace
 
--- Ignia.Topics.Editor.Mvc/Areas/Editor/Components/TextViewComponent.cs
+++ Ignia.Topics.Editor.Mvc/Areas/Editor/Components/TextViewComponent.cs
@@ -3,6 +3,13 @@
 | Client        Ignia, LLC
 | Project       Topics Library
 \=============================================================================================================================*/
+using Ignia.Topics.Editor.Models;
+using Ignia.Topics.Editor.Models.Components;
+using Ignia.Topics.Editor.Models.Components.ViewModels;
+using Ignia.Topics.Editor.Models.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace Ignia.Topics.Editor.Mvc.Components {
 
@@ -10,9 +17,9 @@ namespace Ignia.Topics.Editor.Mvc.Components {
   | CLASS: TEXT (VIEW COMPONENT)
   \---------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
-  ///   Delivers a view model for a form field attribute type.
+  ///   Delivers a view model for a text area attribute type.
   /// </summary>
-  public class TextViewComponent : DefaultAttributeTypeViewComponent {
+  public class TextViewComponent: AttributeTypeViewComponentBase {
 
     /*==========================================================================================================================
     | CONSTRUCTOR
@@ -20,7 +27,49 @@ namespace Ignia.Topics.Editor.Mvc.Components {
     /// <summary>
     ///   Initializes a new instance of a <see cref="TextViewComponent"/> with necessary dependencies.
     /// </summary>
+    /// <returns>A <see cref="TextViewComponent"/>.</returns>
     public TextViewComponent(ITopicRoutingService topicRoutingService) : base(topicRoutingService) { }
 
+    /*==========================================================================================================================
+    | METHOD: INVOKE (ASYNC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Assembles the view model for the <see cref="DefaultAttributeTypeViewComponent"/>.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync(
+      TextAttributeTopicViewModel attribute,
+      string htmlFieldPrefix
+    ) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set HTML prefix
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      ViewData.TemplateInfo.HtmlFieldPrefix = htmlFieldPrefix;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var viewModel = new AttributeViewModel<TextAttributeTopicViewModel>(attribute);
+
+      GetAttributeViewModel(viewModel);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Set configuration values
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      attribute.MaximumLength   ??= attribute.GetIntegerConfigurationValue(     "MaximumLength",        Int32.MaxValue);
+      attribute.CssClass        ??= attribute.GetConfigurationValue(            "CssClass",             "FormField Field");
+
+      attribute.InputType       ??= attribute.GetBooleanConfigurationValue("ValidateEmail", false)? "email" : null;
+      attribute.InputType       ??= attribute.GetBooleanConfigurationValue("ValidatePhone", false)? "tel" : null;
+      attribute.InputType       ??= "text";
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return view with view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return View(viewModel);
+
+    }
+
   } // Class
+
 } // Namespace
