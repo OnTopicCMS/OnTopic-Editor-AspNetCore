bad41b4ffa55781874f0620bd64035ddcb96d4a1
JeremyCaney <jeremy.caney@ignia.com>
2020-04-21 15:15:52 -0700

    Updated to comply with Ignia's latest style guide, formatting standards
    As part of the lead-up to OnTopic 4.0.0, most of the scripts were updated to use Ignia's latest style guide. For some reason, however, the `TokenizedTopicList` was overlooked, and continued to use legacy comment blocks, and comment block indentation. The file has been updated to use the latest standards, for consistency with other script files.

--- OnTopic.Editor.AspNetCore/Shared/Scripts/TokenizedTopicList.js
+++ OnTopic.Editor.AspNetCore/Shared/Scripts/TokenizedTopicList.js
@@ -1,95 +1,87 @@
 /*==============================================================================================================================
-| TOKENIZED TOPIC LIST
-|
-| Author        Katherine Trunkey, Ignia LLC (Katherine.Trunkey@ignia.com)
-| Client        Ignia
-| Project       Topics Editor
-|
-| Purpose       Provides a "class" wrapper with associated properties set as part of its constructor, as well the
-|               getTokenizedTopics() prototypical extension method, used for instantiating the Token-Input plugin.
-|
->===============================================================================================================================
-| Revisions     Date            Author                          Comments
-| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-|               04.11.15        Katherine Trunkey               Moved Token-Input instantiation out of inline client-side code
-|               04.14.15        Katherine Trunkey               Refactored to TokenizedTopics class structure
-\-----------------------------------------------------------------------------------------------------------------------------*/
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
 
 /*==============================================================================================================================
 | TOKENIZED TOPICS CLASS
+>-------------------------------------------------------------------------------------------------------------------------------
+| Provides a "class" wrapper with associated properties set as part of its constructor, as well the getTokenizedTopics()
+| prototypical extension method, used for instantiating the Token-Input plugin.
 \-----------------------------------------------------------------------------------------------------------------------------*/
-  var TokenizedTopics           = function() {
+var TokenizedTopics             = function() {
 
-    'use strict';
+  'use strict';
 
   /*----------------------------------------------------------------------------------------------------------------------------
   | Constructor/internal properties
   \---------------------------------------------------------------------------------------------------------------------------*/
-    this.selector               = '';
-    this.scope                  = '';
-    this.attributeName          = '';
-    this.attributeValue         = '';
-    this.searchProperty         = 'text';
-    this.queryParameter         = 'Query';
-    this.selectedTopics         = '';
-    this.resultLimit            = '';
-    this.tokenLimit             = 100;
-    this.isAutoPostBack         = false;
+  this.selector                 = '';
+  this.scope                    = '';
+  this.attributeName            = '';
+  this.attributeValue           = '';
+  this.searchProperty           = 'text';
+  this.queryParameter           = 'Query';
+  this.selectedTopics           = '';
+  this.resultLimit              = '';
+  this.tokenLimit               = 100;
+  this.isAutoPostBack           = false;
 
-  };
+};
 
 /*==============================================================================================================================
 | GET TOKENIZED TOPICS (TOKENIZED TOPICS EXTENSION METHOD)
 \-----------------------------------------------------------------------------------------------------------------------------*/
-  TokenizedTopics.prototype.getTokenizedTopics = function() {
+TokenizedTopics.prototype.getTokenizedTopics = function() {
 
   /*----------------------------------------------------------------------------------------------------------------------------
   | Scope TokenizedTopics properties to local variable
   \---------------------------------------------------------------------------------------------------------------------------*/
-    var self                    = this;
+  var self                      = this;
 
   /*----------------------------------------------------------------------------------------------------------------------------
   | Build Topics.Json.aspx call URL
   \---------------------------------------------------------------------------------------------------------------------------*/
-    var topicsUrl               = '/OnTopic/Json/'       +
-      self.scope                                         +
-      '?ShowRoot=true'                                   +
-      '&FlattenStructure=true'                           +
-      '&UsePartialMatch=true'                            +
-      '&AttributeName='         + self.attributeName     +
-      '&AttributeValue='        + self.attributeValue    +
-      '&ResultLimit='           + self.resultLimit;
+  var topicsUrl                 = '/OnTopic/Json/'              +
+    self.scope                                                  +
+    '?ShowRoot=true'                                            +
+    '&FlattenStructure=true'                                    +
+    '&UsePartialMatch=true'                                     +
+    '&AttributeName='           + self.attributeName            +
+    '&AttributeValue='          + self.attributeValue           +
+    '&ResultLimit='             + self.resultLimit;
 
   /*----------------------------------------------------------------------------------------------------------------------------
   | Initialize Token-Input with options set on TokenizedTopics
   \---------------------------------------------------------------------------------------------------------------------------*/
-    $(self.selector).tokenInput(topicsUrl, {
-      propertyToSearch          : this.searchProperty,
-      queryParam                : this.queryParameter,
-      minChars                  : 3,
-      enableHTML                : true,
-      tokenLimit                : self.tokenLimit,
-      preventDuplicates         : true,
-      prePopulate               : self.selectedTopics,
-      theme                     : 'facebook',
-      onAdd: function (item) {
-        if (!self.isAutoPostBack) return;
-        $("form").validate().cancelSubmit = true;
-        $("form").submit();
-      },
-      resultsFormatter          : function(item) {
-        var   breadcrumbs       = item.path;
-        breadcrumbs             = breadcrumbs.substring(0, breadcrumbs.indexOf(item.key)).replace(new RegExp(':', 'g'), ': ');
-        breadcrumbs             = breadcrumbs.replace(new RegExp('([a-z])([A-Z])', 'g'), '$1 $2');
-        if (breadcrumbs.indexOf('Root:') >= 0) {
-          breadcrumbs           = breadcrumbs.substring(breadcrumbs.indexOf('Root:') + 5);
-        }
-        return '' +
-          '<li>' +
-          '  <small class="Breadcrumbs">' + breadcrumbs + '</small>' +
-          '  <div class="Selection">' + item.text + '</div>' +
-          '</li>';
+  $(self.selector).tokenInput(topicsUrl, {
+    propertyToSearch            : this.searchProperty,
+    queryParam                  : this.queryParameter,
+    minChars                    : 3,
+    enableHTML                  : true,
+    tokenLimit                  : self.tokenLimit,
+    preventDuplicates           : true,
+    prePopulate                 : self.selectedTopics,
+    theme                       : 'facebook',
+    onAdd                       : function (item) {
+      if (!self.isAutoPostBack) return;
+      $("form").validate().cancelSubmit = true;
+      $("form").submit();
+    },
+    resultsFormatter            : function(item) {
+      var breadcrumbs           = item.path;
+      breadcrumbs               = breadcrumbs.substring(0, breadcrumbs.indexOf(item.key)).replace(new RegExp(':', 'g'), ': ');
+      breadcrumbs               = breadcrumbs.replace(new RegExp('([a-z])([A-Z])', 'g'), '$1 $2');
+      if (breadcrumbs.indexOf('Root:') >= 0) {
+        breadcrumbs             = breadcrumbs.substring(breadcrumbs.indexOf('Root:') + 5);
       }
-    });
+      return '' +
+        '<li>' +
+        '  <small class="Breadcrumbs">' + breadcrumbs + '</small>' +
+        '  <div class="Selection">' + item.text + '</div>' +
+        '</li>';
+    }
+  });
 
-  };
+};
