2b644b913a608da3614bce5420ea87157a9e50ba
JeremyCaney <jeremy.caney@ignia.com>
2020-04-21 16:10:40 -0700

    Introduced new `OnTopic.SelectableTreeView` JavaScript class
    Centralized the configuration for the relationships into a new `OnTopic.SelectableTreeView` JavaScript control, similar to what was previously done with `OnTopic.Navigation` (e6a93b7) and `OnTopic.NestedTopics` (33b4d1f).
    
    This makes the view cleaner, keeps the JavaScript in a JavaScript class which is easier to validate and debug, and (as a minor improvement) ensures the code isn't duplicated in the markup if multiple relationships are on a page (rare).

---
+++ OnTopic.Editor.AspNetCore/Shared/Scripts/SelectableTreeView.js
@@ -0,0 +1,119 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+Ext.namespace('OnTopic');
+
+/*==============================================================================================================================
+| CLASS: SELECTABLE TREE VIEW
+\-----------------------------------------------------------------------------------------------------------------------------*/
+/**
+ * @class OnTopic.SelectableTreeView
+ * @extends Ext.tree.TreePanel
+ * Provides an implementation of a {@link Ext.tree.TreePanel} which exposes checkboxes and is backed by a hidden field for
+ * collecting the topic identifiers of any nodes checked. For use with e.g. the relationships view component.
+ * @constructor
+ * Create new SelectableTreeView object directly.
+ * @param {object} options (optional) Options to overwrite either the {@link OnTopic.SelectableTreeView} or the underlying
+ * {@link Ext.tree.TreePanel}.
+ */
+OnTopic.SelectableTreeView = Ext.extend(Ext.tree.TreePanel, {
+
+  /*============================================================================================================================
+  | DEFINE LOCAL FIELDS
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  checkAscendants               : false,
+  selectedTopics                : null,
+  backingField                  : null,
+
+  /*============================================================================================================================
+  | METHOD: SELECT TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  selectTopic                   : function(node) {
+    node.checked                = true;
+    node.select();
+    return true;
+  },
+
+  /*============================================================================================================================
+  | METHOD: UPDATE BACKING FIELD
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  updateBackingField: function (node, checked) {
+    if (checked) {
+      this.selectedTopics.push(node.attributes.id.toString());
+    }
+    else {
+      this.selectedTopics.remove(node.attributes.id.toString());
+    }
+    this.backingField.dom.value = this.selectedTopics.concat(",");
+    if (!this.checkAscendants) return true;
+    if (checked && node.parentNode) node.parentNode.getUI().toggleCheck(true);
+  },
+
+  /*============================================================================================================================
+  | CONSTRUCTOR
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  constructor                   : function(options) {
+
+    /*--------------------------------------------------------------------------------------------------------------------------
+    | Set default options based on parameters
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    Ext.apply(this, options, OnTopic.SelectableTreeView.defaults);
+
+    /*--------------------------------------------------------------------------------------------------------------------------
+    | Initialize variables
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    this.backingField           = Ext.get(options.backingField);
+    this.selectedTopics         = this.backingField.dom.value.split(",");
+
+    /*--------------------------------------------------------------------------------------------------------------------------
+    | Call parent constructor
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    OnTopic.SelectableTreeView.superclass.constructor.call(this);
+
+  },
+
+  /*============================================================================================================================
+  | INITIALIZE COMPONENT
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  initComponent                 : function () {
+
+    /*--------------------------------------------------------------------------------------------------------------------------
+    | Call parent initializer
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    OnTopic.SelectableTreeView.superclass.initComponent.call(this);
+
+    /*--------------------------------------------------------------------------------------------------------------------------
+    | Set default listeners
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    var me = this;
+
+    me.on('click',              me.selectTopic,                 this);
+    me.on('checkchange',        me.updateBackingField,          this);
+
+  }
+
+});
+
+/*==============================================================================================================================
+| DEFAULTS
+\-----------------------------------------------------------------------------------------------------------------------------*/
+OnTopic.SelectableTreeView.defaults = {
+  id                            : 'relatedTree',
+  useArrows                     : true,
+  autoScroll                    : true,
+  animate                       : true,
+  enableDD                      : false,
+  containerScroll               : true,
+  border                        : false,
+  baseCls                       : 'RelationshipsTreeView',
+  root                          : new Ext.tree.AsyncTreeNode({
+    checked                     : true,
+    text                        : 'Web',
+    draggable                   : false,
+    id                          : 'related',
+    leaf                        : false
+  }),
+  rootVisible                   : false
+};
