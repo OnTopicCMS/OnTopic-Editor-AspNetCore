3e11b5c50d047a3a9541ae68f56489a3ee127f15
JeremyCaney <jeremy.caney@ignia.com>
2020-12-09 14:35:56 -0800

    Standardized formatting for `AssemblyInfo` classes
    This introduces consistency with other Ignia and OnTopic projects.

--- OnTopic.Editor.AspNetCore/Properties/AssemblyInfo.cs
+++ OnTopic.Editor.AspNetCore/Properties/AssemblyInfo.cs
@@ -1,21 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
 using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+/*==============================================================================================================================
+| DEFINE ASSEMBLY ATTRIBUTES
+>===============================================================================================================================
+| Declare and define attributes used in the compiling of the finished assembly.
+\-----------------------------------------------------------------------------------------------------------------------------*/
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("18159da7-b44e-4681-98c9-f81a2007196b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
--- OnTopic.Editor.Models/Properties/AssemblyInfo.cs
+++ OnTopic.Editor.Models/Properties/AssemblyInfo.cs
@@ -1,14 +1,24 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+/*==============================================================================================================================
+| DEFINE ASSEMBLY ATTRIBUTES
+>===============================================================================================================================
+| Declare and define attributes used in the compiling of the finished assembly.
+\-----------------------------------------------------------------------------------------------------------------------------*/
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("02a3d98a-bf8a-4150-8fec-45a0d9c322e4")]
 
-// Handle suppressions
+/*==============================================================================================================================
+| HANDLE SUPPRESSIONS
+>===============================================================================================================================
+| Suppress warnings from code analysis that are either false positives or not relevant for this assembly.
+\-----------------------------------------------------------------------------------------------------------------------------*/
 [assembly: SuppressMessage("Naming", "CA1721:Property names should not match get methods", Justification = "By design", Scope = "member", Target = "~P:OnTopic.Editor.Models.Metadata.ContentTypeDescriptorTopicViewModel.AttributeDescriptors")]
 [assembly: SuppressMessage("Naming", "CA1721:Property names should not match get methods", Justification = "By design", Scope = "member", Target = "~P:OnTopic.Editor.Models.Components.BindingModels.AttributeBindingModel.Value")]
