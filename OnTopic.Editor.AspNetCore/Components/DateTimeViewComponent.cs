1bf89856090d36602252a5616e09f9d2b6935caf
JeremyCaney <jeremy.caney@ignia.com>
2021-01-20 14:02:24 -0800

    Remove legacy configuration support for `DateTimeAttribute`
    By removing the `GetConfigurationValue()` calls, we do away with the legacy ASP.NET Web Forms User Control style configuration support, which was previously maintained for backward compatibility. As part of this, we effectively move the default values from the view component to the attribute topic view model, which is a more logical place to store the defaults. This will also allow us, eventually, to migrate the attribute topic view model to an immutable, read-only object—and ideally a C# 9.0 `record`.

--- OnTopic.Editor.AspNetCore/Components/DateTimeViewComponent.cs
+++ OnTopic.Editor.AspNetCore/Components/DateTimeViewComponent.cs
@@ -57,17 +57,6 @@ namespace OnTopic.Editor.AspNetCore.Components {
       var model = new DateTimeAttributeViewModel(currentTopic, attribute);
 
       /*------------------------------------------------------------------------------------------------------------------------
-      | Set configuration values
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      attribute.IncludeDatePicker       ??= attribute.GetBooleanConfigurationValue("IncludeDatePicker", true);
-      attribute.IncludeTimePicker       ??= attribute.GetBooleanConfigurationValue("IncludeTimePicker", true);
-      attribute.DateFormat              ??= attribute.GetConfigurationValue("DateFormat", "yy-mm-dd");
-      attribute.TimeFormat              ??= attribute.GetConfigurationValue("TimeFormat", "hh:mm tt");
-      attribute.DateTimeSeparator       ??= attribute.GetConfigurationValue("DateTimeSeparator", " ");
-      attribute.DateTimeOffset          ??= attribute.GetIntegerConfigurationValue("DateTimeOffset", 0);
-      attribute.DateTimeOffsetUnits     ??= attribute.GetConfigurationValue("DateTimeOffsetUnits", "Days");
-
-      /*------------------------------------------------------------------------------------------------------------------------
       | Return view with view model
       \-----------------------------------------------------------------------------------------------------------------------*/
       return View(model);
