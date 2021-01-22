98b2a04323a7e4a0adf32594b870fdef3939ff46
JeremyCaney <jeremy.caney@ignia.com>
2021-01-21 19:50:23 -0800

    Updated `AttributeBindingModelLookupService` to be dynamic
    Instead of manually registering the binding models—which, eventually, won't be located in a referenced assembly—instead search all loaded assemblies for classes which derived from the `AttributeBindingModel` record. This will pave the way to support distributing attribute types as plugins.

--- OnTopic.Editor.AspNetCore/Infrastructure/AttributeBindingModelLookupService.cs
+++ OnTopic.Editor.AspNetCore/Infrastructure/AttributeBindingModelLookupService.cs
@@ -15,7 +15,7 @@ namespace OnTopic.Editor.AspNetCore.Infrastructure {
   ///   Provides a mapping between string and class names to be used when mapping <see cref="Topic"/> to a <see
   ///   cref="AttributeBindingModel"/> or derived class.
   /// </summary>
-  public class AttributeBindingModelLookupService : StaticTypeLookupService {
+  public class AttributeBindingModelLookupService : DynamicTypeLookupService {
 
     /*==========================================================================================================================
     | CONSTRUCTOR
@@ -24,28 +24,7 @@ namespace OnTopic.Editor.AspNetCore.Infrastructure {
     ///   Estabblishes a new instance of the <see cref="EditorBindingModelLookupService"/>.
     /// </summary>
     /// <returns>A new instance of the <see cref="EditorBindingModelLookupService"/>.</returns>
-    public AttributeBindingModelLookupService() : base() {
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Add Editor-specific view models
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      Add(typeof(BooleanAttributeBindingModel));
-      Add(typeof(DateTimeAttributeBindingModel));
-      Add(typeof(FileListAttributeBindingModel));
-      Add(typeof(FilePathAttributeBindingModel));
-      Add(typeof(HtmlAttributeBindingModel));
-      Add(typeof(IncomingRelationshipAttributeBindingModel));
-      Add(typeof(InstructionAttributeBindingModel));
-      Add(typeof(LastModifiedAttributeBindingModel));
-      Add(typeof(LastModifiedByAttributeBindingModel));
-      Add(typeof(NestedTopicListAttributeBindingModel));
-      Add(typeof(NumberAttributeBindingModel));
-      Add(typeof(RelationshipAttributeBindingModel));
-      Add(typeof(TextAttributeBindingModel));
-      Add(typeof(TextAreaAttributeBindingModel));
-      Add(typeof(TokenizedTopicListAttributeBindingModel));
-      Add(typeof(TopicListAttributeBindingModel));
-      Add(typeof(TopicReferenceAttributeBindingModel));
+    public AttributeBindingModelLookupService() : base(t => typeof(AttributeBindingModel).IsAssignableFrom(t)) {
 
     }
 
