f7c994e452f4a7c828a61b0f061065eb6db1013b
JeremyCaney <jeremy.caney@ignia.com>
2021-02-15 12:40:28 -0800

    Moved attribute-specific build tasks to `Attributes` project
    With the attribute-specific client-side resources now being registered with npm in the `OnTopic.Editor.AspNetCore.Attributes` project, the `gulpFile.js` now needs to be forked between the two main projects, with attribute-specific items maintained in the `Attributes` copy.
    
    Unfortunately, this introduces a lot of duplication of code in the `gulpFile.js`. In a future task, we'll likely want to find a way to centralize this code so that it can be shared between these two projects. Until that point, however, this establishes a framework for two distinct build processes, one for the editor interface, and another for the attribute plugins.

--- OnTopic.Editor.AspNetCore.Attributes/gulpFile.js
+++ OnTopic.Editor.AspNetCore.Attributes/gulpFile.js
@@ -40,26 +40,16 @@ const files = {
   scss                          : 'Shared/Styles/**/[!_]*.scss',
   js                            : 'Shared/Scripts/*.js',
   vendor                        : {
-    js                          :   [ 'node_modules/jquery/dist/jquery.min.js',
-                                      'node_modules/jquery-ui-dist/jquery-ui.min.js',
-                                      'node_modules/jquery-validation/dist/jquery.validate.min.js',
-                                      'node_modules/jquery-validation/dist/additional-methods.min.js',
-                                      'node_modules/popper.js/dist/umd/popper.min.js',
-                                      'node_modules/bootstrap/dist/js/bootstrap.min.js',
-                                      'Shared/Scripts/ExtJS/ext-base.js',
-                                      'Shared/Scripts/ExtJS/ext-all.js',
-                                      'Shared/Scripts/ExtJS/ext-ExtendTextField.js',
-                                      'node_modules/jquery.are-you-sure/jquery.are-you-sure.js'
-                                    ],
-  css                           :   [ 'node_modules/jquery-ui-dist/jquery-ui.min.css'
+    js                          :   [ 'node_modules/jquery-tokeninput/dist/js/jquery-tokeninput.min.js',
+                                      'node_modules/jquery-ui-timepicker-addon/dist/jquery-ui-timepicker-addon.min.js'                                                                          ],
+  css                           :   [ 'node_modules/jquery-tokeninput/dist/css/token-input.min.css',
+                                      'node_modules/jquery-tokeninput/dist/css/token-input-facebook.min.css',
+                                      'node_modules/jquery-ui-timepicker-addon/dist/jquery-ui-timepicker-addon.min.css'
                                     ]
                                   },
-  precompiled                   : {
+  standalone                    : {
     'Scripts'                   :   {
-      'ExtJS'                   :     'Shared/Scripts/ExtJS/*.js'
-                                    },
-    'Styles': {
-      'ExtJS'                   :     'Shared/Scripts/ExtJS/Resources/**/*'
+      'CkEditor'                :     'Shared/Scripts/CkEditor/*.js'
                                     }
                                   }
 };
@@ -161,18 +151,25 @@ var batchSetFactory = (source, destination, factory) => {
 };
 
 /*==============================================================================================================================
-| TASK: PRECOMPILED FILES
+| TASK: STANDALONE FILES
 >-------------------------------------------------------------------------------------------------------------------------------
-| Copies precompiled dependencies from their source folders and into their appropriate build folders.
+| Copies static dependencies from their source folders and into their appropriate build folders.
 \-----------------------------------------------------------------------------------------------------------------------------*/
-var precompiledFilesTask = () => {
+var standaloneFilesTask = () => {
   var streams = [];
-  for (var contentType in files.precompiled) {
+  for (var contentType in files.standalone) {
+    var factory = (function (contentType) {
+      switch (contentType) {
+        case 'Scripts'          : return jsFactory;
+        case 'Styles'           : return scssFactory;
+        case 'Fonts'            : return copyFilesFactory;
+      }
+    })(contentType);
     streams.push(
       batchSetFactory(
-        files.precompiled[contentType],
+        files.standalone[contentType],
         getOutputDir(contentType),
-        copyFilesFactory
+        factory
       )
     );
   }
@@ -198,14 +195,14 @@ exports.js                      = jsTask;
 exports.scss                    = scssTask;
 exports.jsVendor                = jsVendorTask;
 exports.cssVendor               = cssVendorTask;
-exports.precompiledFiles        = precompiledFilesTask;
+exports.standaloneFiles         = standaloneFilesTask;
 
 /*==============================================================================================================================
 | TASK: BUILD
 >-------------------------------------------------------------------------------------------------------------------------------
 | Composite task that will call all build-related tasks.
 \-----------------------------------------------------------------------------------------------------------------------------*/
-exports.build = parallel(precompiledFilesTask, scssTask, cssVendorTask, jsTask, jsVendorTask);
+exports.build = parallel(standaloneFilesTask, scssTask, cssVendorTask, jsTask, jsVendorTask);
 
 /*==============================================================================================================================
 | TASK: DEFAULT
@@ -213,4 +210,4 @@ exports.build = parallel(precompiledFilesTask, scssTask, cssVendorTask, jsTask, 
 | The default task when Gulp runs, assuming no task is specified. Assuming the environment variable isn't explicitly defined
 | otherwise, will run on development-oriented tasks.
 \-----------------------------------------------------------------------------------------------------------------------------*/
-exports.default = parallel(precompiledFilesTask, scssTask, cssVendorTask, jsTask, jsVendorTask);
+exports.default = parallel(standaloneFilesTask, scssTask, cssVendorTask, jsTask, jsVendorTask);
