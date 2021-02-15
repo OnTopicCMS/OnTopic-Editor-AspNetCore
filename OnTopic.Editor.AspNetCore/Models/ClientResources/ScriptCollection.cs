e40f95a42197083d8d5f65390fe26cea0a01fb8e
JeremyCaney <jeremy.caney@ignia.com>
2021-02-15 11:51:12 -0800

    Introduced `ScriptResource` and `ScriptCollection`
    The `ScriptResource` and `ScriptCollection` derive from `ClientResource` and `ClientResourceCollection<T>`, and introduce an `InHeader` and `IsDeferred` property for determining _how_ and _where_ scripts should be exposed. They provide a strongly typed collection of scripts that should be registered for, and displayed in either the header or the footer (depending on the `InHeader` bit).
    
    The `GetResources()` overload allows the `inHeader` or, optionally, `isDeferred` parameters to be specified, thus filtering the results.

---
+++ OnTopic.Editor.AspNetCore/Models/ClientResources/ScriptCollection.cs
@@ -0,0 +1,48 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace OnTopic.Editor.AspNetCore.Models.ClientResources {
+
+  /*============================================================================================================================
+  | CLASS: SCRIPT COLLECTION
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents a collection of <see cref="ScriptResource"/> instances.
+  /// </summary>
+  public class ScriptCollection: ClientResourceCollection<ScriptResource> {
+
+    /*==========================================================================================================================
+    | METHOD: REGISTER
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Registers a client-side resource.
+    /// </summary>
+    public void Register(Uri url, bool inHeader = false, bool isDeferred = true) {
+      if (!Contains(url)) {
+        Add(
+          new() {
+            Url                 = url,
+            InHeader            = inHeader,
+            IsDeferred          = isDeferred
+          }
+        );
+      }
+    }
+
+    /*==========================================================================================================================
+    | METHOD: GET RESOURCES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Retrieves a collection of resources, optionally filtering by
+    /// </summary>
+    public ReadOnlyCollection<ScriptResource> GetResources(bool inHeader, bool? isDeferred = null) =>
+      new(GetResources().Where(r => r.InHeader == inHeader && (isDeferred is null || r.IsDeferred == isDeferred)).ToList());
+
+  }
+}
