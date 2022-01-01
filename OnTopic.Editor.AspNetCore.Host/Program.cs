3257d18dc0667faa478a094507b2a258a6783d30
JeremyCaney <Jeremy.Caney@Ignia.com>
2022-01-01 11:53:48 -0800

    Fixed indentation with `Program`
    With the ASP.NET Core 6 Hosting Model now in place, we can now unify the indentation, taking full advantage of C# 9 top-level statements.

--- OnTopic.Editor.AspNetCore.Host/Program.cs
+++ OnTopic.Editor.AspNetCore.Host/Program.cs
@@ -11,78 +11,78 @@ using OnTopic.AspNetCore.Mvc;
 using OnTopic.Editor.AspNetCore;
 using OnTopicTest;
 
-    /*==========================================================================================================================
-    | CONFIGURE SERVICES
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    var builder                 = WebApplication.CreateBuilder(args);
+/*==============================================================================================================================
+| CONFIGURE SERVICES
+\-----------------------------------------------------------------------------------------------------------------------------*/
+var builder                 = WebApplication.CreateBuilder(args);
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: Cookie Policy
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      builder.Services.Configure<CookiePolicyOptions>(options => {
-        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-        options.CheckConsentNeeded = context => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-      });
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: Cookie Policy
+\-----------------------------------------------------------------------------------------------------------------------------*/
+builder.Services.Configure<CookiePolicyOptions>(options => {
+  // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+  options.CheckConsentNeeded = context => true;
+  options.MinimumSameSitePolicy = SameSiteMode.None;
+});
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: MVC
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      var mvcBuilder = builder.Services.AddControllersWithViews()
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: MVC
+\-----------------------------------------------------------------------------------------------------------------------------*/
+var mvcBuilder = builder.Services.AddControllersWithViews()
 
-        //Add OnTopic support
-        .AddTopicSupport()
+  //Add OnTopic support
+  .AddTopicSupport()
 
-        //Add OnTopic editor support
-        .AddTopicEditor();
+  //Add OnTopic editor support
+  .AddTopicEditor();
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: Runtime View Compilation
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      if (builder.Environment.IsDevelopment()) {
-        mvcBuilder.AddRazorRuntimeCompilation();
-        builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(options => {
-          var libraryPath = Path.GetFullPath(Path.Combine(builder.Environment.ContentRootPath, "..", "OnTopic.Editor.AspNetCore"));
-          var pluginsPath = Path.GetFullPath(Path.Combine(builder.Environment.ContentRootPath, "..", "OnTopic.Editor.AspNetCore.Attributes"));
-          options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
-          options.FileProviders.Add(new PhysicalFileProvider(pluginsPath));
-        });
-      }
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: Runtime View Compilation
+\-----------------------------------------------------------------------------------------------------------------------------*/
+if (builder.Environment.IsDevelopment()) {
+  mvcBuilder.AddRazorRuntimeCompilation();
+  builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(options => {
+    var libraryPath = Path.GetFullPath(Path.Combine(builder.Environment.ContentRootPath, "..", "OnTopic.Editor.AspNetCore"));
+    var pluginsPath = Path.GetFullPath(Path.Combine(builder.Environment.ContentRootPath, "..", "OnTopic.Editor.AspNetCore.Attributes"));
+    options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
+    options.FileProviders.Add(new PhysicalFileProvider(pluginsPath));
+  });
+}
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Register: Activators
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      var activator = new SampleActivator(builder.Configuration.GetConnectionString("OnTopic"), builder.Environment);
+/*------------------------------------------------------------------------------------------------------------------------------
+| Register: Activators
+\-----------------------------------------------------------------------------------------------------------------------------*/
+var activator = new SampleActivator(builder.Configuration.GetConnectionString("OnTopic"), builder.Environment);
 
-      builder.Services.AddSingleton<IControllerActivator>(activator);
-      builder.Services.AddSingleton<IViewComponentActivator>(activator);
+builder.Services.AddSingleton<IControllerActivator>(activator);
+builder.Services.AddSingleton<IViewComponentActivator>(activator);
 
-    /*==========================================================================================================================
-    | CONFIGURE APPLICATION
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    var app                     = builder.Build();
+/*==============================================================================================================================
+| CONFIGURE APPLICATION
+\-----------------------------------------------------------------------------------------------------------------------------*/
+var app                     = builder.Build();
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: Error Pages
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      if (app.Environment.IsDevelopment()) {
-        app.UseDeveloperExceptionPage();
-      }
-      else {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: Error Pages
+\-----------------------------------------------------------------------------------------------------------------------------*/
+if (app.Environment.IsDevelopment()) {
+  app.UseDeveloperExceptionPage();
+}
+else {
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
 
-     /*------------------------------------------------------------------------------------------------------------------------
-     | Configure: Server defaults
-     \-----------------------------------------------------------------------------------------------------------------------*/
-      //app.UseHttpsRedirection();
-      app.UseStaticFiles();
-      app.UseRouting();
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: Server defaults
+\-----------------------------------------------------------------------------------------------------------------------------*/
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: MVC
-      \-----------------------------------------------------------------------------------------------------------------------*/
-        app.MapControllers();
-        app.MapTopicEditorRoute();
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: MVC
+\-----------------------------------------------------------------------------------------------------------------------------*/
+app.MapControllers();
+  app.MapTopicEditorRoute();
