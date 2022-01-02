8ebb409afa134b7ec9fe8149d33a1637040fdf5d
JeremyCaney <Jeremy.Caney@Ignia.com>
2022-01-02 11:39:33 -0800

    Established `OnTopic.Attributes` as a global using directive
    This isn't widely used now, but will be when the `AttributeDictionary` constructor overload is introduced. In anticipation of that, this is being declared globally within the `Attributes` projects, and the existing local using directives are being removed.

--- OnTopic.Editor.AspNetCore.Attributes/TokenizedTopicListAttribute/TokenizedTopicListAttributeDescriptor.cs
+++ OnTopic.Editor.AspNetCore.Attributes/TokenizedTopicListAttribute/TokenizedTopicListAttributeDescriptor.cs
@@ -3,7 +3,6 @@
 | Client        Ignia, LLC
 | Project       Topics Library
 \=============================================================================================================================*/
-using OnTopic.Attributes;
 using OnTopic.Editor.AspNetCore.Attributes.QueryableTopicListAttribute;
 
 namespace OnTopic.Editor.AspNetCore.Attributes.TokenizedTopicListAttribute {
